e25a305c8044eabf6ca47ad629564a97d0a2172d
@alonso_isidoro <alonsoir@gmail.com>
2024-10-02 11:15:04 +0200

    Add files via upload

---
+++ images.py
@@ -0,0 +1,10 @@
+from openai import OpenAI
+client = OpenAI()
+
+response = client.images.generate(
+    prompt="Un dibujo estilo manga de Lisa Manoban",
+    n=2,
+    size="1024x1024"
+)
+
+print(response.data[0].url)
