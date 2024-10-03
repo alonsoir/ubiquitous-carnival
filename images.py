8f0bfe1cf9f3d8e82a05b02515d82dfceb3599ec
@alonso_isidoro <alonsoir@gmail.com>
2024-10-03 18:39:25 +0200

    Add files via upload

--- images.py
+++ images.py
@@ -3,7 +3,8 @@ from openai import OpenAI
 client = OpenAI()
 
 response = client.images.generate(
-    prompt="Un dibujo estilo manga de Lisa Manoban", n=2, size="1024x1024"
+    prompt="Un cuadro de nubes y niebla sobre el mar, al amanecer, realista", n=2, size="1024x1024"
 )
 
-print(response.data[0].url)
+for i, image in enumerate(response.data):
+    print(f"Imagen {i+1}: {image.url}")
