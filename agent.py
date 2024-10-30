5a7de3b4cff90575311e81a82ad787da5670411f
@alonso_isidoro <alonsoir@gmail.com>
2024-10-30 10:52:57 +0100

    Add files via upload

--- agent.py
+++ agent.py
@@ -4,7 +4,31 @@ from langchain_openai import ChatOpenAI
 from langgraph.graph import START, StateGraph, MessagesState
 from langgraph.prebuilt import tools_condition, ToolNode
 from dotenv import load_dotenv
+from rich.console import Console
+import pyfiglet
+import arrow
+from textblob import TextBlob
+blob_message="Python is amazing!"
+blob = TextBlob(blob_message)
+
+
+now = arrow.now()
+
+
+result = pyfiglet.figlet_format("AGENT 007")
+console = Console()
+
+console.print(f"[red]{result}[/red]")
+
+
+console.print("[red]This is a helpful assistant tasked with writing performing arithmetic on a set of inputs.[/red]")
+console.print("[red]You can use the following tools:[/red]")
+console.print("[blue]add, multiply, divide[/blue]")
+console.print("[yellow]The tools will be invoked by the llm.[/yellow]")
+console.print(f"Today is {now.shift(hours=+0).format('YYYY-MM-DD HH:mm:ss')}")
 load_dotenv()
+console.print(f"This is the blob_message: {blob_message}. [blue]Calculate the sentiment of the blob_message[/blue]")
+console.print(f"{blob.sentiment}")  # Outputs the sentiment of the text
 
 def add(a: int, b: int) -> int:
     """Adds a and b.
@@ -15,6 +39,7 @@ def add(a: int, b: int) -> int:
     """
     return a + b
 
+
 def multiply(a: int, b: int) -> int:
     """Multiplies a and b.
 
@@ -23,6 +48,7 @@ def multiply(a: int, b: int) -> int:
         b: second int
     """
     return a * b
+
 
 def divide(a: int, b: int) -> float:
     """Adds a and b.
@@ -33,6 +59,7 @@ def divide(a: int, b: int) -> float:
     """
     return a / b
 
+
 tools = [add, multiply, divide]
 
 # Define LLM with bound tools
@@ -40,11 +67,15 @@ llm = ChatOpenAI(model="gpt-4o")
 llm_with_tools = llm.bind_tools(tools)
 
 # System message
-sys_msg = SystemMessage(content="You are a helpful assistant tasked with writing performing arithmetic on a set of inputs.")
+sys_msg = SystemMessage(
+    content="You are a helpful assistant tasked with writing performing arithmetic on a set of inputs."
+)
+
 
 # Node
 def assistant(state: MessagesState):
-   return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
+    return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
+
 
 # Build graph
 builder = StateGraph(MessagesState)
@@ -61,14 +92,22 @@ builder.add_edge("tools", "assistant")
 
 # Compile graph
 graph = builder.compile()
-# Run graph
-output = graph.invoke({"messages": [HumanMessage(content="What is 2 + 2?")]})
+
+first_message="What is 2 + 2?"
+console.print(f"[blue]{first_message}[/blue]")
+output = graph.invoke({"messages": [HumanMessage(content=first_message)]})
 for m in output["messages"][-1:]:
-    m.pretty_print()
-# Run graph
-output = graph.invoke({"messages": [HumanMessage(content="What is 2 * 3?")]})
+    console.print(m.pretty_print())
+
+second_message="What is 2 * 3?"
+console.print(f"[blue]{second_message}[/blue]")
+output = graph.invoke({"messages": [HumanMessage(content=second_message)]})
 for m in output["messages"][-1:]:
-    m.pretty_print()
-output = graph.invoke({"messages": [HumanMessage(content="What is 2 / 2?")]})
+    console.print(m.pretty_print())
+
+third_message="What is 2 / 2?"
+console.print(f"[blue]{third_message}[/blue]")
+output = graph.invoke({"messages": [HumanMessage(content=third_message)]})
 for m in output["messages"][-1:]:
-    m.pretty_print()
+    console.print(m.pretty_print())
+
